4c15e8306d4c0c4ed855cca1ee009212e040eb7a
ormaman-svg <or.maman@gmail.com>
2026-06-24 06:58:32 +0300

    revert: delete i18n use-translation file

--- src/lib/i18n/use-translation.ts
+++
@@ -1,7 +0,0 @@
-import { useLang } from "./context";
-import { translations, type TranslationKeys } from "./translations";
-
-export function useT(): TranslationKeys {
-  const { lang } = useLang();
-  return translations[lang];
-}
